4befa79f320a32fc84caa0356d916b4beaaa253a
tomhostyn <tom.hostyn@gmail.com>
2016-02-08 21:38:52 +0100

    implement cost function

--- Week 2/machine-learning-ex1/ex1/computeCost.m
+++ Week 2/machine-learning-ex1/ex1/computeCost.m
@@ -13,9 +13,9 @@ J = 0;
 % Instructions: Compute the cost of a particular choice of theta
 %               You should set J to the cost.
 
-
-
-
+H = X * theta;
+d = (H - y) .^ 2;
+J = 1/(2*m) * sum(d);
 
 % =========================================================================
 
