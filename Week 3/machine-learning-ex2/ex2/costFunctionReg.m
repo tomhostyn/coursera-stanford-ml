c2c20dab8d4867413f773a9cd55bb13f8a57178f
tomhostyn <tom.hostyn@gmail.com>
2016-02-12 17:21:33 +0100

    regularized logistic regression

--- Week 3/machine-learning-ex2/ex2/costFunctionReg.m
+++ Week 3/machine-learning-ex2/ex2/costFunctionReg.m
@@ -17,8 +17,8 @@ grad = zeros(size(theta));
 %               Compute the partial derivatives and set grad to the partial
 %               derivatives of the cost w.r.t. each parameter in theta
 
-
-
+[J, grad] = costFunction(theta, X, y);
+J = J + lambda/(2*m)* sum (theta(2:end) .^2);
 
 
 
