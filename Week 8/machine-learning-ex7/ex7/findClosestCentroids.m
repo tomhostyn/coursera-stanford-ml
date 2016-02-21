a6a31260d5a6a1bdab742cf14e3331cea7875b61
tomhostyn <tom.hostyn@gmail.com>
2016-02-21 19:38:08 +0100

    cosest centroids k-means

--- Week 8/machine-learning-ex7/ex7/findClosestCentroids.m
+++ Week 8/machine-learning-ex7/ex7/findClosestCentroids.m
@@ -21,11 +21,10 @@ idx = zeros(size(X,1), 1);
 % Note: You can use a for-loop over the examples to compute this.
 %
 
-
-
-
-
-
+for i = 1:size(X,1)
+all_norms = (sqrt(sum(((X(i,:) - centroids)').^2)));
+[m idx(i)] = min (all_norms);
+end
 
 % =============================================================
 
