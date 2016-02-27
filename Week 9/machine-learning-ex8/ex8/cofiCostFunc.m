7a94314579ddbc7da6b2a7c823203c902f968e1d
tomhostyn <tom.hostyn@gmail.com>
2016-02-27 13:05:23 +0100

    regularized cost function

--- Week 9/machine-learning-ex8/ex8/cofiCostFunc.m
+++ Week 9/machine-learning-ex8/ex8/cofiCostFunc.m
@@ -41,6 +41,8 @@ Theta_grad = zeros(size(Theta));
 %
 
 J = sum (sum ((((X * Theta') - Y) .* R) .^2) / 2);
+J = J + lambda/2 * sum (sum ( Theta .^2))
+J = J + lambda/2 * sum (sum ( X .^2))
 
 X_grad = (((X * Theta') - Y) .* R) * Theta;
 Theta_grad = (((X * Theta') - Y) .* R)' * X;
